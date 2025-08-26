35d91608d56919dcea825c65de616dd8b6ad09cd
Marcelo Borjas <70559579+gborjasb@users.noreply.github.com>
2025-08-26 11:19:50 -0500

    Revert "Runge-Kutta Method"
    This reverts commit 3f04eb3ce3add0450234ebfefee6000f2ee46e55.

--- test.py
+++ test.py
@@ -14,20 +14,3 @@ def primos(int a, int b):
 # Cambios
 
 # Mas Cambios
-
-def RungeKuttaMethod(f, y0, t0, t1, h):
-    n = int((t1 - t0) / h)
-    t = t0
-    y = y0
-    for i in range(n):
-        k1 = h * f(t, y)
-        k2 = h * f(t + h / 2, y + k1 / 2)
-        k3 = h * f(t + h / 2, y + k2 / 2)
-        k4 = h * f(t + h, y + k3)
-        y += (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        t += h
-    return y
-
-
-# Lineas nuevas
-# Otra Linea Nueva
